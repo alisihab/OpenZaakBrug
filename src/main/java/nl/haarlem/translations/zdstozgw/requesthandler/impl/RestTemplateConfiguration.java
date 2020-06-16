68b8885ec8909f3fdd9fae9e943a1acb7b5cc704
aschultz <aschultz@haarlem.nl>
2020-06-16 08:32:15 +0200

    [WIP] refactoring rename LoggableRestTemplate

--- src/main/java/nl/haarlem/translations/zdstozgw/requesthandler/impl/RestTemplateConfiguration.java
+++ src/main/java/nl/haarlem/translations/zdstozgw/requesthandler/impl/RestTemplateConfiguration.java
@@ -1,20 +1,12 @@
 package nl.haarlem.translations.zdstozgw.requesthandler.impl;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RestTemplateConfiguration {
     @Bean
-    @Qualifier("customRestTemplateCustomizer")
-    public LoggableRestTemplate customRestTemplateCustomizer() {
-        return new LoggableRestTemplate();
+    public LoggingRestTemplate customRestTemplateCustomizer() {
+        return new LoggingRestTemplate();
     }
-
-//    @Bean
-//    @DependsOn(value = {"customRestTemplateCustomizer"})
-//    public RestTemplateBuilder restTemplateBuilder() {
-//        return new RestTemplateBuilder(customRestTemplateCustomizer());
-//    }
 }
