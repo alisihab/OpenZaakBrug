1f3063838d6f8e23b2612cb7da98fe7145cb7186
Eduard Witteveen <eduard@witteveen.frl>
2020-08-26 10:54:23 +0200

    more verbose logging for proxy

--- src/main/java/nl/haarlem/translations/zdstozgw/converter/impl/proxy/Proxy.java
+++ src/main/java/nl/haarlem/translations/zdstozgw/converter/impl/proxy/Proxy.java
@@ -1,5 +1,9 @@
 package nl.haarlem.translations.zdstozgw.converter.impl.proxy;
 
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -25,7 +29,9 @@ import nl.haarlem.translations.zdstozgw.translation.zds.services.ZaakService;
 import nl.haarlem.translations.zdstozgw.utils.XmlUtils;
 
 public class Proxy extends Converter { 
-	
+    
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    
 	public Proxy(RequestHandlerContext context, Translation translation, ZaakService zaakService) {
 		super(context, translation, zaakService);
 	}	
@@ -39,6 +45,10 @@ public class Proxy extends Converter {
 	@Override
 	public ResponseEntity<?> execute() throws ConverterException {
 		var zdsClient= new ZDSClient();
-		return zdsClient.post(this.getContext().getUrl(), this.getContext().getSoapAction(), getContext().getRequestBody());
+		var url = this.getContext().getUrl();
+		var soapaction = this.getContext().getSoapAction();
+		var requestbody = getContext().getRequestBody();
+		log.info("relaying request to url: " + url + " with soapaction: " + soapaction);
+		return zdsClient.post(url, soapaction, requestbody);		
 	}   
 }
