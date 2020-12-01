882a845fce305e90130419acfcdfceb3f2e8ae2d
alisihab <sihab.akcan@gmail.com>
2020-12-01 15:06:13 +0100

    travis build, run in docker and push to hub

---
+++ docker_push.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# error on unset variables, exit on error
+set -eu
+set +x
+
+git_hash=${TRAVIS_COMMIT:-`git rev-parse HEAD`}
+
+# Login to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Echo script commands
+set -x
+
+REPO=sihab/brug
+TAG=${1:-latest}
+
+# Build the image
+docker build \
+    -t $REPO:$TAG \
+    --build-arg COMMIT_HASH=$git_hash \
+    --build-arg RELEASE=$TAG \
+    .
+
+# Push the image
+docker push $REPO:$TAG
