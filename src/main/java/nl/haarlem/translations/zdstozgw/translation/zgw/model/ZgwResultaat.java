f3f352e9d7dd4253761171d10ff48ba9d63def9c
Eduard Witteveen <eduard@witteveen.frl>
2020-12-03 23:31:19 +0100

    [wip] Zaken can be closed, but zaakdocumenten give the error: "Er zijn gerelateerde informatieobjecten waarvoor `indicatieGebruiksrecht` nog niet gespecifieerd is. Je moet deze zetten voor je de zaak kan afsluiten."

--- src/main/java/nl/haarlem/translations/zdstozgw/translation/zgw/model/ZgwResultaat.java
+++ src/main/java/nl/haarlem/translations/zdstozgw/translation/zgw/model/ZgwResultaat.java
@@ -1,4 +1,19 @@
 package nl.haarlem.translations.zdstozgw.translation.zgw.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class ZgwResultaat {
+	@SerializedName("zaak")
+	@Expose	
+	public String zaak;
+	
+	@SerializedName("resultaattype")
+	@Expose		
+	public String resultaattype;
+
+	@SerializedName("toelichting")
+	@Expose		
+	public String toelichting;
+	
 }
